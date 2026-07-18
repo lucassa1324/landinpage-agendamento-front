f233b9fa255532bf54573d32d6aa7a2967505575
Maciel Suassuna <macielsuassuna14@gmail.com>
2026-07-18 14:05:32 -0300

    feat: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${configuredApiUrl}/:path*`,
+        destination: `${configuredApiUrl}/api/:path*`,
       },
     ];
   },
